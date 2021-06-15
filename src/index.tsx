eacdeb34e8bf2998b74c8d09996e7910c6e1628e
leoarmelin <leonardo99.armelin@gmail.com>
2021-06-15 19:10:24 -0300

    feat(Typography): create polymorphic component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,34 @@
-import * as React from 'react'
+import * as React from 'react';
+import styled from 'styled-components';
 
 export interface TypographyProps {
-  text: string
+  text: string;
+  name?:
+    | 'LargeTitle'
+    | 'TitleOne'
+    | 'TitleTwo'
+    | 'TitleThree'
+    | 'Headline'
+    | 'Body'
+    | 'Caption'
+    | 'Footer';
 }
 
-export const Typography = ({ text }: TypographyProps): JSX.Element => {
-  return <h1>{text}</h1>
-}
+const handleType = (typeName: string) => {
+  if (typeName === 'LargeTitle' || typeName === 'TitleOne') return 'h1';
+  if (typeName === 'TitleTwo') return 'h2';
+  if (typeName === 'TitleThree') return 'h3';
+  if (typeName === 'Headline') return 'b';
+  return 'p';
+};
+
+export const Typography = ({
+  text,
+  name = 'Body',
+}: TypographyProps): JSX.Element => {
+  const tagName = handleType(name);
+
+  const Text = styled(tagName)``;
+
+  return <Text>{text}</Text>;
+};
